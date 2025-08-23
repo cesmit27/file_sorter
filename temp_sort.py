72a3818f34f746dcc0a56a2744d1a988c2de3b6f
Colin <164561623+cesmit27@users.noreply.github.com>
2025-08-23 13:46:08 -0400

    Add files via upload

---
+++ temp_sort.py
@@ -0,0 +1,11 @@
+import os
+
+with open('temp.txt', 'r') as f:
+	lines =[line.strip() for line in f]
+
+lines.sort(reverse=True)
+
+with open(temp.txt' , 'w') as f:
+	f.write(','.join(lines))
+
+os.startfile('temp.txt')
